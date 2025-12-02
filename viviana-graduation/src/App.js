7154d296239099ce35cfbfa485cd1ce674112029
tranviviana <tranvivianaedu@gmail.com>
2025-12-02 11:38:57 -0800

    PDF

--- viviana-graduation/src/App.js
+++ viviana-graduation/src/App.js
@@ -41,7 +41,7 @@ function App() {
       
           <h1>Agenda For December 20th Is Available Here</h1>
           <iframe
-  src={"/December_20.pdf"}
+  src="https://drive.google.com/file/d/10ZuQUq1-rKWmh404zeFqSnCb8-oJpXZX/preview"
   style={{ width: "100%", height: "100vh", border: "none" }}
   title="PDF Viewer"
 />
